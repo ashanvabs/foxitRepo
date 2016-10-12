8e4b09967d52fa0d9ddfd399657df15b4da67ada
Ashan Rathnayake <AshanR@embla.lk>
2016-10-12 11:02:03 +0530

    repro

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+export class App {   
+    activate(params, routeConfig, $navigationInstruction) {
+  //      var language = "en-US";
+  //       var newTab = false;
+
+  //       i18n.init(options, function (t) {
+  //           this.viewerDiv.i18n();
+  //           var baseUrl =  getBaseUrl(); //refer to webapp\scripts\control\common\common.js
+  //           seajs.use(['http://localhost:8080/viewer/scripts/release_websdk/webpdf.mini.js?v=20160920'], function (init) {
+  //               var options = {
+  //                   language: language,
+  //                   baseUrl: "http://localhost:8080/",
+  //                   scrollBarType: 0//two type,pc 0,mobile 1
+  //               };
+
+  //               WebPDF.createViewer("docViewer", options);
+
+  //               WebPDF.createAccount(baseUrl, language);
+  //               getCurrentUserInfo(baseUrl, options, newTab, null); //refer to webapp\scripts\control\common\common.js
+  //           });
+
+  //       });
+
+   
+
+    }
+
+    
+
 }
